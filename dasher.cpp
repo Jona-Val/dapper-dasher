5f9d888a177678a3548401d3d07e9c7c80ca6bac
Jonathan_V <jonathan.valdez.contact@gmail.com>
2024-12-18 21:13:28 -0600

    Used for Loop to initialize nebulas

--- dasher.cpp
+++ dasher.cpp
@@ -32,23 +32,22 @@ int main() {
     // Nebula aka hazards to jump
     Texture2D nebula = LoadTexture("textures/12_nebula_spritesheet.png");
 
-    // animData struct for the nebula
-    AnimData nebData{ 
-        {0.0, 0.0, nebula.width/8, nebula.height/8},                    // Rectangle Rec
-        {windowDimensions[0], windowDimensions[1] - nebula.height/8},   // Vector2 Pos
-        0,                                                              // int frame
-        1.0/12.0,                                                       // float updateTime
-        0.0};                                                           // float runningTime
 
-    // animData struct for the nebula
-    AnimData neb2Data{ 
-        {0.0, 0.0, nebula.width/8, nebula.height/8},                            // Rectangle Rec
-        {windowDimensions[0] + 300, windowDimensions[1] - nebula.height/8},     // Vector2 Pos
-        0,                                                                      // int frame
-        1.0/16.0,                                                               // float updateTime
-        0.0};                                                                   // float runningTime
+    AnimData nebulae[2]{};
 
-    AnimData nebulae[2]{ nebData, neb2Data };
+    for (int i = 0; i < 2; i++) {
+        nebulae[i].rec.x = 0.0;
+        nebulae[i].rec.y = 0.0;
+        nebulae[i].rec.width = nebula.width/8;
+        nebulae[i].rec.height = nebula.height/8;
+        nebulae[i].pos.y = windowDimensions[1] - nebula.height/8;
+        nebulae[i].frame = 0;
+        nebulae[i].runningTime = 0.0;
+        nebulae[i].updateTime = 1.0/16.0;
+    }
+
+    nebulae[0].pos.x = windowDimensions[0];
+    nebulae[1].pos.x = windowDimensions[0] + 300;
 
     // nebula X velocity (pixels/second) frame independent
     int nebVel{-200};
